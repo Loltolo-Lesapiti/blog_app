2158a4dcff5174f7e0d37f3c8276289f667683cd
Loltolo-Lesapiti <petrolesapiti@gmail.com>
2022-12-24 18:12:11 +0300

    Remove linters

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '3.1.3'
+ruby '3.1.2'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.0.4'
@@ -37,22 +37,29 @@ gem 'jbuilder'
 # gem "bcrypt", "~> 3.1.7"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
+gem 'tzinfo-data'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', require: false
 
+gem 'rubocop', '>= 1.0', '< 2.0'
 # Use Sass to process CSS
 # gem "sassc-rails"
 
+# Rspec rails
+gem 'rails-controller-testing'
+
+group :development, :test do
+  gem 'rspec-rails', '>= 3.9.0'
+end
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
-gem 'rspec-rails', '~> 4.0.0.beta2'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
 end
-gem 'rails-controller-testing'
 
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,31 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    before(:example) { get users_path(1) }
-    it 'is a success' do
-      expect(response).to have_http_status(:ok)
-      expect(response).to render_template('index')
-      expect(response.body).to include('Users page')
+  describe 'GET/index' do
+    it 'should pass with status 200' do
+      get '/'
+      expect(response.status).to eql(200)
     end
-    it 'renders index template' do
-      expect(response).to render_template('index')
+
+    it 'should render correct template' do
+      get '/'
+      expect(response).to render_template(:index)
     end
-    it 'should include the static text' do
-      expect(response.body).to include('Users page')
+
+    it 'should include correct text' do
+      get '/'
+      expect(response.body).to include('List of users')
     end
   end
 
-  describe 'GET /show' do
-    before(:example) { get user_path(1) }
-    it 'is a success' do
-      expect(response).to have_http_status(:ok)
+  describe 'GET/show' do
+    it 'should pass with status 200' do
+      get '/users/12'
+      expect(response.status).to eql(200)
     end
-    it 'renders show template' do
-      expect(response).to render_template('show')
+
+    it 'should render correct template' do
+      get '/users/12'
+      expect(response).to render_template(:show)
     end
-    it 'should include the static text' do
-      expect(response.body).to include('This is User details')
+
+    it 'should include correct text' do
+      get '/users/12'
+      expect(response.body).to include('List of user by given id')
     end
   end
 end
